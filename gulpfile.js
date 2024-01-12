28f7eded42f0f9c39453d2301fa3a2dbc941f403
lucasrq <lucasroque545@gmail.com>
2024-01-12 03:01:25 -0300

    nmwew

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ const imagemin = require('gulp-imagemin');
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./public/dist/css'));
 }
 
 function minImg() {
     return gulp.src('./src/img/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/imgMin'))
+        .pipe(gulp.dest('./public/dist/imgMin'))
 }
 
 
